3e02b2876f7eb496653ba54d77bc9586bdc73150
Liz Nomura <lizn@hawaii.edu>
2018-02-24 01:22:31 -1000

    removed blocker from select options

--- src/containers/EditForm/index.js
+++ src/containers/EditForm/index.js
@@ -58,7 +58,6 @@ class EditForm extends PureComponent {
             <option value="low">Low</option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
-            <option value="blocker">Blocker</option>
           </select>
           <label htmlFor="createdBy">Created By: </label>
           <input
