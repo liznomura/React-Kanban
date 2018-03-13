23727fcca4a06958f7afc46853e20a4808090398
Liz Nomura <lizn@hawaii.edu>
2018-03-12 16:26:26 -1000

    edited readme and initial popup text

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Hi there! Welcome to my project, React Kanban!
 
-It's a simple kanban board made with:
-- **React.js** (through create-react-app)
+It's a simple Kanban board made with:
+- **React.js** for the UI
 - **Redux** to handle state
+- **Express** as the server
 - **Sass** for styling
 
 Thank you for looking~
--- src/components/initial-popup.js
+++ src/components/initial-popup.js
@@ -8,7 +8,7 @@ class InitialPopup extends PureComponent {
       <div className="popup">
         <div className="popup__content">
           <div className="popup__content--header popup--onload">Welcome!</div>
-          <p>Hi there! Welcome to my project, React Kanban! It's a simple kanban board made with React.js, Redux to handle state, and Sass for styling. Please visit my <a href="https://github.com/liznomura/React-Kanban" target="_blank">Github</a> to see the code! Thanks for visiting!</p>
+          <p>Hi there! Welcome to my project, React Kanban! It's a simple Kanban board made with React for the UI, Redux to handle state, Express as the server, and Sass for styling. Please visit my <a href="https://github.com/liznomura/React-Kanban" target="_blank">Github</a> to see the code! Thanks for visiting!</p>
           <button className="btn popup-close-btn--onload" onClick={this.props.togglePopup}>Got it!</button>
         </div>
       </div>
